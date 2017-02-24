15662ace62b33f8c3ee2b7c86f5de2b3d6d116eb
Sean Smith <seans@auctionedge.com>
2017-02-24 02:03:29 -0800

    Added missing piece to gww''s appWatcher bit. Take 2

--- 3rdparty.lua
+++ 3rdparty.lua
@@ -22,6 +22,19 @@ end
 configFileWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
 configFileWatcher:start()
 
+-- Callback function for application events
+function applicationWatcher(appName, eventType, appObject)
+  if (eventType == hs.application.watcher.activated) then
+    if (appName == "Finder") then
+      -- Bring all Finder windows forward when one gets activated
+      appObject:selectMenuItem({"Window", "Bring All to Front"})
+    end
+  end
+end
+
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
+
 
 -- -------------------------------------------------------------------------------------
 -- Thanks to vic for the smart app laucher
